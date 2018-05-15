7c4fc20bc667fc5405551f3e0d6230dd91566ad0
Bopp <lukasbopp@gmail.com>
2018-05-15 14:03:01 +0200

    added cross origin

--- src/main/java/ch/banshay/taskmanagement/mongo/repositories/TaskRepository.java
+++ src/main/java/ch/banshay/taskmanagement/mongo/repositories/TaskRepository.java
@@ -4,9 +4,11 @@ import ch.banshay.taskmanagement.mongo.models.Task;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 
+@CrossOrigin
 @RepositoryRestResource(collectionResourceRel = "tasks", path = "tasks")
 public interface TaskRepository extends MongoRepository<Task, String> {
   
